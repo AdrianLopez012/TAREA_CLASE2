86e265b3c5dcb140c8b00b37d90aa403ce8bf8a5
Al4cr4n <148933724+Al4cr4n@users.noreply.github.com>
2024-09-08 09:54:09 -0500

    Add files via upload

---
+++ Tarea_2_Gtics/src/test/java/com/example/tarea_2_gtics/Tarea2GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea_2_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
